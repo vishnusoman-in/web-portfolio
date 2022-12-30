b42edeae2baff17692d8cd0151ec9500b122d45d
vishnusoman-in <vishnusoman.pro@gmail.com>
2022-12-29 23:35:58 -0800

    base structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import './App.css';
+import { BrowserRouter, Navigate, Routes, Route, Switch } from "react-router-dom";
+
+import Portfoliopage from "webpages/Portfolio";
 
 function App() {
   return (
     <div className="App">
+
+<BrowserRouter>        
+
+<Routes>
+
+  <Route path="/" element={<Portfoliopage/>} />
+  
+</Routes>
+
+</BrowserRouter>
      
     </div>
   );
---
+++ src/webpages/Portfolio/Portfoliopage.js
@@ -0,0 +1,26 @@
+//import Navbar from "components/Navbar.jsx"
+
+import { useState, useEffect } from "react";
+
+import { useNavigate } from "react-router-dom";
+
+import {Box,Typography,useMediaQuery,Divider} from "@mui/material";
+
+const Portfoliopage = () => { 
+
+   const isDesktop = useMediaQuery("(min-width: 1000px)");
+   const navigate = useNavigate();
+   // onClick={() => {navigate("/");}}
+    
+    return (
+       
+<Box >
+
+
+
+
+</Box>
+
+)};
+
+export default Portfoliopage;
